a31da9c649e7cc15fe1833da4accf841eb7c01ef
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-01-31 12:45:25 -0500

    characterlist working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharacterCard from "./components/CharacterCard";
+import CharacterList from "./components/CharacterList";
 
 export default function App() {
   return (
-    <main>
-      <Header />
-    </main>
-  );
+		<main>
+			<Header />
+     
+      <CharacterList/>
+		</main>
+	);
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div>
+      <img src={props.image}></img>
+      <h2>{props.name}</h2>
+      <p>Species: {props.species}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Status: {props.status}</p>
+    </div>
+
+  );
 }
